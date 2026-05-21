ec07c5c005b8c7377e27c6f96722e5b0f6b559e4
Dhyan Raj <dhyanraj@gmail.com>
2026-05-21 11:44:01 +0000

    Update to v2.2.3

--- Formula/mcp-mesh.rb
+++ Formula/mcp-mesh.rb
@@ -1,18 +1,18 @@
 class McpMesh < Formula
   desc "Distributed service orchestration framework built on the Model Context Protocol"
   homepage "https://github.com/dhyansraj/mcp-mesh"
-  url "https://github.com/dhyansraj/mcp-mesh/releases/download/v2.2.2/mcp-mesh_v2.2.2_darwin_arm64.tar.gz"
-  sha256 "161ab46be3a5def11e48624c7f96bf8288d6436343c97c75a8649eb2a0163460"
+  url "https://github.com/dhyansraj/mcp-mesh/releases/download/v2.2.3/mcp-mesh_v2.2.3_darwin_arm64.tar.gz"
+  sha256 "5d6dc8f691bba75abc060cf307f1f7615bf06a5660e038482b50da6763bcc3f9"
   license "MIT"
-  version "2.2.2"
+  version "2.2.3"
 
   # Dependencies
   depends_on "go" => :build
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/dhyansraj/mcp-mesh/releases/download/v2.2.2/mcp-mesh_v2.2.2_darwin_amd64.tar.gz"
-      sha256 "77e6672da0641f2b6f6a264904f5a33543906950685c25832fececd49d35a393"
+      url "https://github.com/dhyansraj/mcp-mesh/releases/download/v2.2.3/mcp-mesh_v2.2.3_darwin_amd64.tar.gz"
+      sha256 "610d8930eb555bcd03b1a07f0ff74266aa6029203b74cfa6075e6051e8a07c0b"
     end
   end
 
